14dbc10f5bf4c4ab942e7d6056e740fc212cb736
Mikhail Vesialou <mishaismenska@gmail.com>
2020-04-27 19:09:56 +0300

    exel output

--- src/com/mishaismenska/minimizationalgorithm/Main.java
+++ src/com/mishaismenska/minimizationalgorithm/Main.java
@@ -1,17 +1,21 @@
 package com.mishaismenska.minimizationalgorithm;
 
 import javafx.util.Pair;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
 
 
     private static final char emptySet = 'Ø';
 
-    
     private static void print(char[] s){
         for (char c : s) {
             System.out.print(c);
@@ -71,9 +75,29 @@ public class Main {
         return  resultingSet;
     }
 
+    private static XSSFSheet sheet;
+    private static int sheetJ;
     private static ArrayList<char[]> subtract(char[] a, ArrayList<char[]> b, int pc, int i){
+        Cell cell = null;
+        if(sheet != null){
+            int sheeti;
+            if(i >= sheetJ -1) sheeti = i+2;
+            else sheeti = i+1;
+            if(sheet.getRow(sheeti) == null) sheet.createRow(sheeti);
+            Row row = sheet.getRow(sheeti);
+            if(row.getCell(sheetJ) == null) row.createCell(sheetJ);
+            cell = row.getCell(sheetJ);
+        }
         ArrayList<char[]> cr = subtract(a, b.get(i), pc);
-        if(i == b.size() - 1) return cr;
+        if (cell != null) {
+            for(char[] s : cr) {
+                cell.setCellValue(cell.getStringCellValue() + "\n" + charArrayToString(s));
+            }
+        }
+
+        if(i == b.size() - 1){
+            return cr;
+        }
         if(cr.size() == 1){
             return subtract(cr.get(0), b, pc, i+1);
         } else{
@@ -163,6 +187,7 @@ public class Main {
 
 
     public static ArrayList<char[]> findSimpleImplicants(int pc, ArrayList<char[]> L, ArrayList<char[]> N){
+        XSSFWorkbook workbook = new XSSFWorkbook();
         ArrayList<char[]> c = new ArrayList<>();
         c.addAll(L);
         c.addAll(N);
@@ -172,6 +197,7 @@ public class Main {
         ArrayList<char[]> A = new ArrayList<>();
         int ct = 1;
         do{
+            XSSFSheet tableSheet = workbook.createSheet("C table " + ct);
             Z.clear();
             A.clear();
             table.clear();
@@ -181,13 +207,17 @@ public class Main {
                     table.get(i).add(multiply(c.get(i), c.get(j), pc));
                 }
             }
-
+            Row row = tableSheet.createRow(c.size() + 1);
+            row.createCell(0).setCellValue("A");
             for(int i = 0; i < table.size(); i++){
                 for(int j = 0; j < table.get(i).size(); j++){
                     char[] element = table.get(i).get(j);
                     if(element[0] != emptySet){
-                        if(ct == cubeType(element, pc) && !contains(element, A))
-                            A.add(table.get(i).get(j));
+                        if(ct == cubeType(element, pc)){
+                            if(!contains(element, A)) A.add(table.get(i).get(j));
+                            if(row.getCell(j+1) == null) row.createCell(j+1);
+                            row.getCell(j+1).setCellValue(row.getCell(j+1).getStringCellValue() + "\n" + charArrayToString(element));
+                        }
                     }
                 }
             }
@@ -211,33 +241,75 @@ public class Main {
                     Z.add(c.get(i));
                 }
             }
+            int rowCount = 0;
+            row = tableSheet.createRow(rowCount);
+            row.createCell(0).setCellValue("");
+            for(int i = 1; i <= c.size(); i++){
+                row.createCell(i).setCellValue(charArrayToString(c.get(i - 1)));
+            }
+
+            for(int i = 0; i < c.size(); i++){
+                row = tableSheet.createRow(i+1);
+                row.createCell(0).setCellValue(charArrayToString(c.get(i)));
+                for(int j = 0; j < table.get(i).size(); j++){
+                    row.createCell(j+1).setCellValue(charArrayToString(table.get(i).get(j)));
+                }
+                row.createCell(table.get(i).size() + 1).setCellValue("-");
+            }
             ArrayList<char[]> b = subtractSets(c, Z, pc);
             c = combineSets(A, b, pc);
             zs.addAll(Z);
             ct++;
         }while (!A.isEmpty());
-
+        try (FileOutputStream outputStream = new FileOutputStream("Ctables.xlsx")) {
+            workbook.write(outputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return zs;
     }
 
-    public static void findLExtremals(ArrayList<char[]> L, ArrayList<char[]> Z, int pc){
+    private static int indexOf(char[] s, ArrayList<char[]> a){
+        for(int i = 0; i < a.size(); i++){
+            boolean ok = true;
+            for(int j = 0; j < s.length; j++){
+                if(a.get(i)[j] != s[j]){
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok) return i;
+        }
+        return -1;
+    }
+    public static ArrayList<char[]> findLExtremals(ArrayList<char[]> L, ArrayList<char[]> Z, int pc) throws Exception {
         ArrayList<char[]> subtrahend = new ArrayList<>();
+        XSSFWorkbook xw = new XSSFWorkbook();
+        sheet = xw.createSheet("Z subtract Z - Zi");
         //fission, extremal
         ArrayList<Pair<char[], char[]>> fissions = new ArrayList<>();
         for(int i = 0; i < Z.size(); i++){
+            sheetJ = i+1;
             subtrahend.clear();
             subtrahend.addAll(Z);
             subtrahend.remove(i);
             ArrayList<char[]> fission = subtract(Z.get(i), subtrahend, pc, 0);
-            print(Z.get(i));
             for(char[] s : fission){
                 fissions.add(new Pair<>(s, Z.get(i)));
             }
-            System.out.println("fission");
-            printArrayList(fission);
-            System.out.println("------------------------------");
+        }
+        Row row = sheet.createRow(0);
+        for(int i = 0; i < Z.size(); i++){
+            row.createCell(i+1).setCellValue(charArrayToString(Z.get(i)));
+        }
+        for(int i = 0; i < Z.size(); i++){
+            row = sheet.getRow(i+1);
+            row.createCell(0).setCellValue(charArrayToString(Z.get(i)));
         }
 
+        sheet = null;
         char[][][] table = new char[fissions.size()][L.size()][];
 
         for(int i = 0; i < fissions.size(); i++){
@@ -246,24 +318,23 @@ public class Main {
             }
         }
 
+        XSSFSheet oneMoreTable = xw.createSheet("IntersectTable");
+        row = oneMoreTable.createRow(0);
+        for(int i = 0; i < L.size(); i++){
+            row.createCell(i+1).setCellValue(charArrayToString(L.get(i)));
+        }
         for(int i = 0; i < fissions.size(); i++){
+            row = oneMoreTable.createRow(i+1);
+            row.createCell(0).setCellValue(charArrayToString(fissions.get(i).getKey()));
             for(int j = 0; j < L.size(); j++){
-                int k = 0;
-                for(; k < table[i][j].length; k++){
-                    System.out.print(table[i][j][k]);
-                }
-                for(;k <= pc; k++){
-                    System.out.print(" ");
-                }
-                System.out.print("\t");
+                row.createCell(j+1).setCellValue(charArrayToString(table[i][j]));
             }
-            System.out.println("\n");
         }
         ArrayList<char[]> E = new ArrayList<>();
 
         if(fissions.isEmpty()) {
             Random r = new Random();
-            int index = r.nextInt() % Z.size();
+            int index = Math.abs(r.nextInt()) % Z.size();
             E.add(Z.get(index));
         }else{
             for(int i = 0; i < fissions.size(); i++){
@@ -280,15 +351,12 @@ public class Main {
                 }
             }
         }
-        System.out.println("E");
-        printArrayList(E);
 
         ArrayList<char[]> fissionsLsE = new ArrayList<>();
+        sheet = xw.createSheet("L#E");
         for(char[] l : L){
             ArrayList<char[]> fission = subtract(l, E, pc, 0);
             if(!fission.isEmpty()){
-                System.out.println("fuck");
-                print(l);
                 fissionsLsE.addAll(fission);
             }
         }
@@ -300,8 +368,6 @@ public class Main {
             }
         }
 
-        System.out.println("zwe");
-        printArrayList(ZWithoutE);
         char[][][] tableZWEintLsE = new char[ZWithoutE.size()][fissionsLsE.size()][];
         ArrayList<ArrayList<char[]>> tableZWEintLsENotForOutput = new ArrayList<>();
         for(int i = 0; i < ZWithoutE.size(); i++){
@@ -312,93 +378,88 @@ public class Main {
                 if(ne[0] != emptySet) tableZWEintLsENotForOutput.get(i).add(ne);
             }
         }
-        System.out.println("table");
-        for(int i = 0; i < ZWithoutE.size(); i++){
-            for(int j = 0; j < fissionsLsE.size(); j++) {
-                int k = 0;
-                for(; k < tableZWEintLsE[i][j].length; k++){
-                    System.out.print(tableZWEintLsE[i][j][k]);
-                }
-                for(;k <= pc; k++){
-                    System.out.print(" ");
-                }
-                System.out.print("\t");
-            }
-            System.out.println();
+        XSSFSheet oneMoreSheet = xw.createSheet("Z' intersect L1");
+        row = oneMoreSheet.createRow(0);
+        for(int i = 0; i < fissionsLsE.size(); i++){
+            row.createCell(i+1).setCellValue(charArrayToString(fissionsLsE.get(i)));
         }
+        for(int i = 0; i < ZWithoutE.size(); i++){
+            row = oneMoreSheet.createRow(i+1);
+            row.createCell(0).setCellValue(charArrayToString(ZWithoutE.get(i)));
+            for(int j = 0; j < fissionsLsE.size(); j++){
+                row.createCell(j+1).setCellValue(charArrayToString(tableZWEintLsE[i][j]));
+            }
+        }
+        try (FileOutputStream outputStream = new FileOutputStream("Ztables.xlsx")) {
+            xw.write(outputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return E;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ArrayList<char[]> L = new ArrayList<>();
         ArrayList<char[]> N = new ArrayList<>();
-        /*L.add("00000".toCharArray());
-        L.add("11001".toCharArray());
-        L.add("00010".toCharArray());
-        L.add("10010".toCharArray());
-        L.add("10110".toCharArray());
-        L.add("01101".toCharArray());
-        L.add("00100".toCharArray());
-        N.add("00011".toCharArray());
-        N.add("00110".toCharArray());
-        N.add("00111".toCharArray());
-        N.add("01111".toCharArray());
-        N.add("11101".toCharArray());*/
+        L.add("001001".toCharArray());
+        L.add("001010".toCharArray());
+        L.add("001011".toCharArray());
+        L.add("001101".toCharArray());
+        L.add("010100".toCharArray());
+        L.add("011001".toCharArray());
+        L.add("011010".toCharArray());
+        L.add("011011".toCharArray());
+        L.add("011101".toCharArray());
+        L.add("100100".toCharArray());
+        L.add("101100".toCharArray());
+        L.add("110100".toCharArray());
+        L.add("111100".toCharArray());
+        N.add("000110".toCharArray());
+        N.add("000111".toCharArray());
+        N.add("001110".toCharArray());
+        N.add("001111".toCharArray());
+        N.add("010110".toCharArray());
+        N.add("010111".toCharArray());
+        N.add("011110".toCharArray());
+        N.add("011111".toCharArray());
+        N.add("100000".toCharArray());
+        N.add("100001".toCharArray());
+        N.add("100010".toCharArray());
+        N.add("100011".toCharArray());
+        N.add("100101".toCharArray());
+        N.add("100110".toCharArray());
+        N.add("100111".toCharArray());
+        N.add("101000".toCharArray());
+        N.add("101001".toCharArray());
+        N.add("101010".toCharArray());
+        N.add("101011".toCharArray());
+        N.add("101101".toCharArray());
+        N.add("101110".toCharArray());
+        N.add("101111".toCharArray());
+        N.add("110000".toCharArray());
+        N.add("110001".toCharArray());
+        N.add("110010".toCharArray());
+        N.add("110011".toCharArray());
+        N.add("110101".toCharArray());
+        N.add("110110".toCharArray());
+        N.add("110111".toCharArray());
+        N.add("111000".toCharArray());
+        N.add("111001".toCharArray());
+        N.add("111010".toCharArray());
+        N.add("111011".toCharArray());
+        N.add("111101".toCharArray());
+        N.add("111110".toCharArray());
+        N.add("111111".toCharArray());
 
-
-
-        L.add("0000".toCharArray());
-        L.add("0001".toCharArray());
-        L.add("0101".toCharArray());
-        L.add("1100".toCharArray());
-        L.add("1101".toCharArray());
-        L.add("1000".toCharArray());
-
-        /*L.add("00000".toCharArray());
-        L.add("00001".toCharArray());
-        L.add("00010".toCharArray());
-        L.add("00011".toCharArray());
-        L.add("00100".toCharArray());
-        L.add("00101".toCharArray());
-        L.add("01000".toCharArray());
-        L.add("01001".toCharArray());
-        L.add("01010".toCharArray());
-        L.add("01011".toCharArray());
-        L.add("01100".toCharArray());
-        L.add("01101".toCharArray());
-        L.add("10000".toCharArray());
-        L.add("10001".toCharArray());
-        L.add("10010".toCharArray());
-        L.add("10011".toCharArray());
-        L.add("10101".toCharArray());
-        L.add("11000".toCharArray());
-        L.add("11001".toCharArray());
-        L.add("11010".toCharArray());
-        L.add("11011".toCharArray());
-        L.add("11101".toCharArray());
-        L.add("00110".toCharArray());
-        L.add("00111".toCharArray());
-        L.add("01110".toCharArray());
-        L.add("01111".toCharArray());
-        L.add("10110".toCharArray());
-        L.add("10111".toCharArray());
-        L.add("11110".toCharArray());
-        L.add("11111".toCharArray());*/
-/*        L.add("1010".toCharArray());
-        L.add("0010".toCharArray());
-        L.add("0110".toCharArray());
-        L.add("0010".toCharArray());
-        L.add("0000".toCharArray());
-        L.add("0101".toCharArray());
-        L.add("0001".toCharArray());
-        L.add("1110".toCharArray());
-        L.add("1110".toCharArray());
-        L.add("1101".toCharArray());
-        L.add("1001".toCharArray());*/
-        ArrayList<char[]> zs = findSimpleImplicants(4, L, N);
-        findLExtremals(L, zs, 4);
-        //System.out.println("ZS");
-        //printArrayList(zs);
-        //System.out.println(cubeType("00x1x".toCharArray(), 5));
-        //print(subtract("1xx".toCharArray(), "000".toCharArray(), 3).get(0));
+        ArrayList<char[]> zs = findSimpleImplicants(6, L, N);
+        ArrayList<char[]> r = findLExtremals(L, zs, 6);
+        System.out.println();
+        System.out.println();
+        printArrayList(zs);
+        System.out.println();
+        System.out.println();
+        printArrayList(r);
     }
 }
